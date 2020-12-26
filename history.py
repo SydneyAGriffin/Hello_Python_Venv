70f97a15aea4fec991a61ba8cc567727c3c31cc4
SydneyAGriffin <59980876+SydneyAGriffin@users.noreply.github.com>
2020-12-26 13:47:03 -0800

    commit
    Commit

---
+++ history.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sun Jun  7 00:20:06 2020)---
+conda install spyder=4.1.3
+python
+
+## ---(Sun Jun  7 00:55:19 2020)---
+3*5
+conda install spyder=4.1.3
+
+## ---(Sun Jun  7 01:37:55 2020)---
+print('Hello world')
+runfile('C:/Users/Sydne/.spyder-py3/temp.py', wdir='C:/Users/Sydne/.spyder-py3')
+print('Hello world')
+
+## ---(Sun Jun  7 18:38:07 2020)---
+get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+
+## ---(Sun Jun  7 19:04:35 2020)---
+runfile('C:/Users/Sydne/.spyder-py3/temp.py', wdir='C:/Users/Sydne/.spyder-py3')
+import sys
+        !{sys.executable} --version
+import sys
+!{sys.executable} --version
+$ pip --version
+pip --version
+
+## ---(Sun Jun  7 19:16:01 2020)---
+runfile('C:/Users/Sydne/.spyder-py3/temp.py', wdir='C:/Users/Sydne/.spyder-py3')
+mkdir PythonApp
+mkdir sgriffin
+virtualenv sgriffinVenv
+sudo apt-get install python-pip python-dev build-essential
+pip install virtualenv
+mkdir sgriffin
+virtualenv sgriffinVenv
+virtualenv sgriffin
+sgriffinvenv
+virtualenv PythonAppVenv
+source sgriffinVenv/bin/activate
+source sgriffin/bin/activate
+
+## ---(Sun Jun  7 23:44:52 2020)---
+py -m venv env
+
+## ---(Mon Jul 27 02:58:19 2020)---
+runfile('C:/Users/Sydne/.spyder-py3/temp.py', wdir='C:/Users/Sydne/.spyder-py3')
+runcell(0, 'C:/Users/Sydne/.spyder-py3/temp.py')
